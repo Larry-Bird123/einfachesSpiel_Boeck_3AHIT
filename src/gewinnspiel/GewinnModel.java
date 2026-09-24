8e7ff38eff0f11a0a6791bbcd608427078b65f67
Benjamin Böck <bboeck@student.tgm.ac.at>
2026-09-24 08:34:09 +0200

    model: Attribute, Konstruktor und Getter fertig :)

--- src/gewinnspiel/GewinnModel.java
+++ src/gewinnspiel/GewinnModel.java
@@ -1,4 +1,35 @@
 package gewinnspiel;
 
+import java.util.Random;
+
+/**
+ * Model: enthält den Spielzustand und die Spiellogik.
+ * Kennt weder View noch Controller.
+ */
 public class GewinnModel {
+    public static final int STARTPUNKTE = 30;
+    public static final int SIEGPUNKTE = 100;
+
+    private int gesamtPunkte;
+    private int spielerZahl;
+    private int computerZahl;
+    private int rundenErgebnis;
+    private Random random = new Random();
+
+    /** Erzeugt ein neues Spiel mit 30 Startpunkten. */
+    public GewinnModel() {
+        gesamtPunkte = STARTPUNKTE;
+    }
+
+    public int getGesamtPunkte() {
+        return gesamtPunkte;
+    }
+
+    public int getComputerZahl() {
+        return computerZahl;
+    }
+
+    public int getRundenErgebnis() {
+        return rundenErgebnis;
+    }
 }
