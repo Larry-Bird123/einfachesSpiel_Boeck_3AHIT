64aa065a43c95130c95b64bcea39b391c56396aa
Benjamin Böck <bboeck@student.tgm.ac.at>
2026-09-24 08:18:41 +0200

    Projektgrundgerüst mit leeren Klassen und angepasstem .gitignore

---
+++ src/gewinnspiel/Main.java
@@ -0,0 +1,10 @@
+package gewinnspiel;
+
+/**
+ * Startklasse des Zahlen-Gewinnspiels.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // wird später mit dem Controller befüllt
+    }
+}
